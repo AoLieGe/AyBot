32294c820fbe673060c6ba0acaa50fa3486f4e66
olgert_a <56453942+AoLieGe@users.noreply.github.com>
2021-10-07 01:33:31 +0500

    fix

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ subs = SubscriptionList()
 sub_provider = SubscriptionProvider(subs)
 sub_provider.load()
 
-#subs.add('heyayvaz', AYGUILD, AYCHANNEL, True)  #temp solution
+
 
 stream_checker = Checker(TWITCH_ID, TWITCH_SECRET, discord_client, sub_provider)
 
@@ -35,6 +35,7 @@ cmd.add_parser(StreamCmd(sub_provider))
 async def on_ready():
     print('Logged on as {0}!'.format(discord_client.user))
     stream_checker.check.start()
+    subs.add('heyayvaz', AYGUILD, AYCHANNEL, True)  #temp solution
 
 
 @discord_client.event
