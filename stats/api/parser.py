9ea53ab6a46dd3bc9cd213fb940a65c1c8e92a6a
Oleg <56453942+AoLieGe@users.noreply.github.com>
2022-01-29 09:56:17 +0500

    fix

--- stats/api/parser.py
+++ stats/api/parser.py
@@ -1,6 +1,6 @@
 import asyncio
 import aiohttp
-from datetime import date
+from datetime import datetime
 from stats.api.request import StatsRequest as Api
 from stats.api.misc import *
 
@@ -53,7 +53,7 @@ class StatsParser:
             data = json.loads(resp)
             timestamp = data['last_match']['opened']
             players_data = data['last_match']['players']
-            time = date.fromtimestamp(timestamp)
+            time = datetime.fromtimestamp(timestamp)
             time_str = time.strftime("%d.%m.%Y %H:%M:%S\n")
             sorted_by_team = [p for p in sorted(players_data, key=lambda item: item['team'])]
             rates = [await StatsParser.rating_by_id(session, data['steam_id'], data['profile_id'])
