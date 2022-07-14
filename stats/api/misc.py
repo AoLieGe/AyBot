4a80f934bef5e7422be3bc4150fb53a3f8f6dcc7
Oleg <56453942+AoLieGe@users.noreply.github.com>
2022-07-14 21:07:45 +0500

    refactor code in rank and match cmds

--- stats/api/misc.py
+++ stats/api/misc.py
@@ -1,5 +1,49 @@
 import enum
 import json
+from datetime import datetime
+
+
+def remove_brackets(resp: str) -> str:
+    """remove start and end brackets ->[data]<- in rating response"""
+    return resp[1:len(resp) - 1]
+
+
+def calc_winrate(wins: int, losses: int) -> int:
+    """calculate winrate using number of wins and number of losses"""
+    if (wins + losses) != 0:
+        return round(100 * wins / (wins + losses))
+    else:
+        return 0
+
+
+def convert_rank(leaderboard: str, status: int, data: str) -> str:
+    """parse rank query results into string"""
+
+    if status == 200:
+        data = remove_brackets(data)
+        try:
+            data = json.loads(data)
+            winrate = calc_winrate(data['num_wins'], data['num_losses'])
+            result = f"{leaderboard}:{data['rating']} {winrate}% {data['streak']}"
+            return result
+
+        except ValueError:
+            return '----'
+    else:
+        return '----'
+
+
+def format_time(timestamp: int) -> str:
+    time = datetime.fromtimestamp(timestamp)
+    return time.strftime("Started: %d.%m.%Y %H:%M:%S UTC+0\n")
+
+
+def format_match_player(data: dict, rank) -> str:
+    try:
+        team = data['color'] if data['team'] != -1 else '-'
+        return f"{team}: {data['name']} {rank}"
+    except ValueError:
+        return '----'
 
 
 def formatted(resp: tuple) -> str:
--- stats/api/parser.py
+++ stats/api/parser.py
@@ -34,38 +34,65 @@ class StatsParser:
 
     @staticmethod
     async def rating_by_id(session: aiohttp.ClientSession, steam_id: str, profile_id: str) -> str:
-        leaderboards = [lb.name for lb in LeaderboardID if lb != LeaderboardID.UNRANKED]
-        if (not steam_id or steam_id == '') and (not profile_id or profile_id == ''):
-            return ' '.join([f'{lb}:----' for lb in leaderboards])
+        leaderboards = [lb for lb in LeaderboardID if lb != LeaderboardID.UNRANKED]
+        tasks = [Api.rating(session, steam_id, profile_id, lb.value) for lb in leaderboards]
+        ranks_raw = await asyncio.gather(*[asyncio.create_task(t) for t in tasks])
+        ranks = [convert_rank(lb.name, status, data) for lb, (status, data) in zip(leaderboards, ranks_raw)]
+        return ' | '.join(ranks)
 
-        tasks = [Api.rating(session, steam_id, profile_id, leaderboard_id=lb.value)
-                 for lb in LeaderboardID if lb != LeaderboardID.UNRANKED]
-        resp = await asyncio.gather(*[asyncio.create_task(t) for t in tasks])
-        json_data = [to_json(formatted(d)) if s == 200 else {} for s, d in resp]
-        rates = [(data['rating'], data['num_wins'], data['num_losses'], data['streak'])
-                 if data != {} else '----' for data in json_data]
-        result = [f'{lb}:{r[0]} {winrate(r[1], r[2])}% {streak(r[3])}'
-                  for lb, r in zip(leaderboards, rates)]
-        return ' | '.join(result)
+        # leaderboards = [lb.name for lb in LeaderboardID if lb != LeaderboardID.UNRANKED]
+        # if (not steam_id or steam_id == '') and (not profile_id or profile_id == ''):
+        #     return ' '.join([f'{lb}:----' for lb in leaderboards])
+        #
+        # tasks = [Api.rating(session, steam_id, profile_id, leaderboard_id=lb.value)
+        #          for lb in LeaderboardID if lb != LeaderboardID.UNRANKED]
+        # resp = await asyncio.gather(*[asyncio.create_task(t) for t in tasks])
+        # json_data = [to_json(formatted(d)) if s == 200 else {} for s, d in resp]
+        # rates = [(data['rating'], data['num_wins'], data['num_losses'], data['streak'])
+        #          if data != {} else '----' for data in json_data]
+        # result = [f'{lb}:{r[0]} {winrate(r[1], r[2])}% {streak(r[3])}'
+        #           for lb, r in zip(leaderboards, rates)]
+        # return ' | '.join(result)
 
     @staticmethod
     async def match_by_id(session: aiohttp.ClientSession, steam_id: str) -> str:
         status, resp = await Api.match(session, steam_id)
         if status != 200:
+
             return f'Error: Request return status {status}'
         try:
             data = json.loads(resp)
-            timestamp = data['last_match']['opened']
-            players_data = data['last_match']['players']
-            time = datetime.fromtimestamp(timestamp)
-            time_str = time.strftime("Started: %d.%m.%Y %H:%M:%S UTC+0\n")
-            sorted_by_team = [p for p in sorted(players_data, key=lambda item: item['team'])]
-            rates = [await StatsParser.rating_by_id(session, data['steam_id'], data['profile_id'])
-                     for data in sorted_by_team]
-            rates_str = '\n'.join([f"{format_team(d['team'])}: {d['name']} {r}" for r, d in zip(rates, sorted_by_team)])
-            return time_str + rates_str
+            time = format_time(data['last_match']['opened'])
+            players_data = sorted(data['last_match']['players'], key=lambda item: (item['team'], item['color']))
+            # get ranks of all players
+            tasks = [StatsParser.rating_by_id(session, player['steam_id'], player['profile_id'])
+                     for player in players_data]
+            ranks = await asyncio.gather(*[asyncio.create_task(t) for t in tasks])
+            match = [format_match_player(data, rank) for data, rank in zip(players_data, ranks)]
+            # build result with timestamp and players rank info
+            result = time + '\n'.join(match)
+
+            return result
         except ValueError:
-            return 'Match parse error: incorrect match json'
+            pass
+        return ""
+
+        # status, resp = await Api.match(session, steam_id)
+        # if status != 200:
+        #     return f'Error: Request return status {status}'
+        # try:
+        #     data = json.loads(resp)
+        #     timestamp = data['last_match']['opened']
+        #     players_data = data['last_match']['players']
+        #     time = datetime.fromtimestamp(timestamp)
+        #     time_str = time.strftime("Started: %d.%m.%Y %H:%M:%S UTC+0\n")
+        #     sorted_by_team = [p for p in sorted(players_data, key=lambda item: item['team'])]
+        #     rates = [await StatsParser.rating_by_id(session, data['steam_id'], data['profile_id'])
+        #              for data in sorted_by_team]
+        #     rates_str = '\n'.join([f"{format_team(d['team'])}: {d['name']} {r}" for r, d in zip(rates, sorted_by_team)])
+        #     return time_str + rates_str
+        # except ValueError:
+        #     return 'Match parse error: incorrect match json'
 
     @staticmethod
     async def _find_player(session: aiohttp.ClientSession, name: str, leaderboard_id: LeaderboardID) -> dict:
