450bf5957473f0bb9794980b060bea3baed7be66
Oleg <56453942+AoLieGe@users.noreply.github.com>
2022-10-11 12:27:44 +0300

    Update cmd.py

--- chat/moderate/cmd.py
+++ chat/moderate/cmd.py
@@ -14,8 +14,12 @@ class ModerateCmd(CmdContainer):
         msg = message.content.lower()
         user = message.author.id
         guild = message.guild.id
+        channel = message.channel.id
 
         if guild != self.guild:
+            return
+        
+        if channel == '822903067233878016'
             return
 
         if user == self.admin:
